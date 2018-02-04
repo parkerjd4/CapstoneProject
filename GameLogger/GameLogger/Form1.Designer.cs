bbed91b83ab601347fc09e38c5c17b85e3bd74e9
Joseph Parker <parkerjd4@appstate.edu>
2018-02-03 21:49:28 -0500

    Fixed Menu Bar

--- GameLogger/GameLogger/Form1.Designer.cs
+++ GameLogger/GameLogger/Form1.Designer.cs
@@ -31,6 +31,7 @@
             this.GameName = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // GameName
@@ -66,11 +67,21 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(85, 181);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
             // From1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(590, 383);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.GameName);
@@ -90,5 +101,6 @@
         private System.Windows.Forms.TextBox GameName;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
--- GameLogger/GameLogger/Form1.cs
+++ GameLogger/GameLogger/Form1.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net.Http;
+using System.IO;
+using System.Diagnostics;
 
 namespace GameLogger
 {
@@ -38,6 +41,15 @@ namespace GameLogger
 
         private void button2_Click(object sender, EventArgs e)
         {
+            Close();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //HttpResponse<T> response = Unirest.get("https://api-2445582011268.apicast.io/characters/?fields=*&limit=10")
+                //.header("user-key", "c707f6bc39116f2006b8a0d61e23175e")
+               // .header("Accept", "application/json").asJson();
+
 
         }
     }
