7c90b754818c6c66a1e29b24c40cb6383c250c31
Joseph Parker <parkerjd4@appstate.edu>
2018-02-21 16:27:26 -0500

    Added more XML tags
    I added developers, publishers, and release date.

--- GameLogger/GameLogger/MainWindow.xaml.cs
+++ GameLogger/GameLogger/MainWindow.xaml.cs
@@ -84,20 +84,32 @@ namespace GameLogger
             var Game = client.GetGame(result.First().Id);
 
             XmlNode GameName = doc.CreateElement("Game_Name");
-            XmlNode Genre = doc.CreateElement("Genre");
             XmlNode Description = doc.CreateElement("Description");
+            XmlNode ReleaseDate = doc.CreateElement("Release_Date");
             XmlNode Platforms = doc.CreateElement("Platforms");
+            XmlNode Genre = doc.CreateElement("Genres");
+            XmlNode Publishers = doc.CreateElement("Publishers");
+            XmlNode Developers = doc.CreateElement("Developers");
+            
 
-            GameName.InnerText = result.First().Name.ToString();
-            Description.InnerText = result.First().Deck.ToString();
+            GameName.InnerText = Game.Name.ToString();
+            Description.InnerText = Game.Deck.ToString();
+            ReleaseDate.InnerText = Game.ExpectedReleaseDay.ToString();
             Genre.InnerText = GetGenre(Game.Genres.ToList());
             Platforms.InnerText = GetPlatforms(Game.Platforms.ToList());
+            Publishers.InnerText = GetPublishers(Game.Publishers.ToList());
+            Developers.InnerText = GetDevelopers(Game.Developers.ToList());
+
 
             node.AppendChild(GameName);
             node.AppendChild(Description);
             node.AppendChild(Genre);
             node.AppendChild(Platforms);
-            
+            node.AppendChild(ReleaseDate);
+            node.AppendChild(Publishers);
+            node.AppendChild(Developers);
+
+
             doc.DocumentElement.AppendChild(node);
             doc.Save(filepath);
         }
@@ -136,6 +148,41 @@ namespace GameLogger
             return y;
         }
 
+        public string GetPublishers(List<Publisher> list)
+        {
+            string y = "";
+            foreach (var x in list)
+            {
+                if (y == "")
+                {
+                    y += x.Name;
+                }
+                else
+                {
+                    y += ", " + x.Name;
+                }
+            }
+            return y;
+        }
+
+        public string GetDevelopers(List<Developer> list)
+        {
+            string y = "";
+            foreach (var x in list)
+            {
+                if (y == "")
+                {
+                    y += x.Name;
+                }
+                else
+                {
+                    y += ", " + x.Name;
+                }
+            }
+            return y;
+        }
+
+
         public void LoadFile(string path)
         {
             XmlDocument doc = new XmlDocument();
@@ -146,7 +193,7 @@ namespace GameLogger
             {
                 string gameName = xn["Game_Name"].InnerText;
                 GameList.Add(gameName);
-                gameImg.Add(new BitmapImage(new Uri(@"C:\Users\Dillon\Source\Repos\CapstoneProject\GameLogger\GameLogger\Properties\placeholder.png")));
+                //gameImg.Add(new BitmapImage(new Uri(@"C:\Users\Dillon\Source\Repos\CapstoneProject\GameLogger\GameLogger\Properties\placeholder.png")));
                 gameListImg.ItemsSource = GameList;
                 //gameListImg.ItemsSource = gameImg;
 
@@ -161,26 +208,15 @@ namespace GameLogger
                 colName.ReadOnly = true;
                 colName.Resizable = System.Windows.Forms.DataGridViewTriState.True;
                 colName.Width = 60;
-                */
-
-
-                /*DataGridTextColumn textColumn = new DataGridTextColumn();
+                DataGridTextColumn textColumn = new DataGridTextColumn();
                 textColumn.Header = gameName;
                 textColumn.Binding = new System.Windows.Data.Binding(gameName);
                 gameListView.Items.Add(new GameData() {GameName = gameName});*/
-
-
-
 
             }
             
           
          }
-
-        private void Menu(object sender, RoutedEventArgs e)
-        {
-
-        }
 
         private void Menu_Click_Add(object sender, RoutedEventArgs e)
         {
@@ -193,14 +229,6 @@ namespace GameLogger
             var systemPath = System.Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData);
             var complete = System.IO.Path.Combine(systemPath, "GameLogger");
             var filepath = System.IO.Path.Combine(complete, "game_list.xml");
-
-
-
-        }
-
-        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-
         }
     }
 }
