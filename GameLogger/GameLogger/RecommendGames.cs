5bed8e35c1b35679d17a0276a0bc55a263c8787d
parkerjd4 <parkerjd4@appstate.edu>
2018-03-31 16:23:25 -0400

    Trying to link the label in recommend games
    GiantBomb is having issues so going to stop now

--- GameLogger/GameLogger/RecommendGames.cs
+++ GameLogger/GameLogger/RecommendGames.cs
@@ -48,25 +48,29 @@ namespace GameLogger
 
         internal void SetTableContents(List<Game> similarGames)
         {
-         
-            GameName1 = similarGames[0].Name.ToString();
-            GameName2 = similarGames[1].Name.ToString();
-            GameName3 = similarGames[2].Name.ToString();
-            GameName4 = similarGames[3].Name.ToString();
-
             linkLabel1.Text = similarGames[0].Name.ToString();
             linkLabel2.Text = similarGames[1].Name.ToString();
             linkLabel3.Text = similarGames[2].Name.ToString();
             linkLabel4.Text = similarGames[3].Name.ToString();
+
             var Client = new GiantBombRestClient("23896f4f00ce753ef98a3c79c42c3d4e226dded0");
+
             var Result1 = Client.SearchForGames(similarGames[0].Name.ToString()).ToList();
             var Game1 = Client.GetGame(Result1.First().Id);
+
             var Result2 = Client.SearchForGames(similarGames[1].Name.ToString()).ToList();
             var Game2 = Client.GetGame(Result2.First().Id);
+
             var Result3 = Client.SearchForGames(similarGames[2].Name.ToString()).ToList();
             var Game3 = Client.GetGame(Result3.First().Id);
+
             var Result4 = Client.SearchForGames(similarGames[4].Name.ToString()).ToList();
             var Game4 = Client.GetGame(Result4.First().Id);
+
+            GameName1 = Game1.SiteDetailUrl.ToString();
+            GameName2 = Game1.SiteDetailUrl.ToString();
+            GameName3 = Game1.SiteDetailUrl.ToString();
+            GameName4 = Game1.SiteDetailUrl.ToString();
 
             string url = Game1.Image.MediumUrl.ToString().Trim();
             string url1 = Game2.Image.MediumUrl.ToString().Trim();
@@ -81,13 +85,14 @@ namespace GameLogger
             var filepath1 = System.IO.Path.Combine(complete, Game2.Id + ".png");
             var filepath2 = System.IO.Path.Combine(complete, Game3.Id + ".png");
             var filepath3 = System.IO.Path.Combine(complete, Game4.Id + ".png");
+
             client.Headers["User-Agent"] = "josedpar";
             client.DownloadFile(new Uri(url), filepath);
-            client.Headers["User-Agent"] = "josedpar1";
+            client.Headers["User-Agent"] = "josedpar123";
             client.DownloadFile(new Uri(url1), filepath1);
-            client.Headers["User-Agent"] = "josedpar2";
+            client.Headers["User-Agent"] = "josedparCAP";
             client.DownloadFile(new Uri(url2), filepath2);
-            client.Headers["User-Agent"] = "josedpar3";
+            client.Headers["User-Agent"] = "josedparSTONE";
             client.DownloadFile(new Uri(url3), filepath3);
 
             pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
@@ -116,6 +121,12 @@ namespace GameLogger
 
         private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
+            var Client = new GiantBombRestClient("23896f4f00ce753ef98a3c79c42c3d4e226dded0");
+            var Result = Client.SearchForGames(GameName1).ToList();
+            var Game = Client.GetGame(Result.First().Id);
+            linkLabel1.LinkVisited = true;
+
+            System.Diagnostics.Process.Start(Game.SiteDetailUrl);
 
         }
 
