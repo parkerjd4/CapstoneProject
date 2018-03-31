2fcc23c08ef05ae924583f668069b305645f6061
parkerjd4 <parkerjd4@appstate.edu>
2018-03-31 14:25:47 -0400

    Got the Deck to appear
    Deck now appears in the recommanded games view

--- GameLogger/GameLogger/RecommendGames.cs
+++ GameLogger/GameLogger/RecommendGames.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using GiantBomb.Api;
 using GiantBomb.Api.Model;
 
 namespace GameLogger
@@ -45,12 +46,22 @@ namespace GameLogger
             linkLabel2.Text = similarGames[1].Name.ToString();
             linkLabel3.Text = similarGames[2].Name.ToString();
             linkLabel4.Text = similarGames[3].Name.ToString();
+            var Client = new GiantBombRestClient("23896f4f00ce753ef98a3c79c42c3d4e226dded0");
+            var Result1 = Client.SearchForGames(similarGames[0].Name.ToString()).ToList();
+            var Game1 = Client.GetGame(Result1.First().Id);
+            var Result2 = Client.SearchForGames(similarGames[1].Name.ToString()).ToList();
+            var Game2 = Client.GetGame(Result2.First().Id);
+            var Result3 = Client.SearchForGames(similarGames[2].Name.ToString()).ToList();
+            var Game3 = Client.GetGame(Result3.First().Id);
+            var Result4 = Client.SearchForGames(similarGames[4].Name.ToString()).ToList();
+            var Game4 = Client.GetGame(Result4.First().Id);
 
-            //label4.Text = Game.Description.ToString(); 
+
+            label4.Text = Game1.Deck.ToString(); 
             //string.Join(Environment.NewLine + "                    ", similarGames[0].Deck.ToString().Split().Select((word, index) => new { word, index }).GroupBy(y => y.index / 9).Select(grp => string.Join(" ", grp.Select(y => y.word))));
-            //label5.Text = string.Join(Environment.NewLine + "                    ", similarGames[1].Deck.ToString().Split().Select((word, index) => new { word, index }).GroupBy(y => y.index / 9).Select(grp => string.Join(" ", grp.Select(y => y.word))));
-            //label6.Text = string.Join(Environment.NewLine + "                    ", similarGames[2].Deck.ToString().Split().Select((word, index) => new { word, index }).GroupBy(y => y.index / 9).Select(grp => string.Join(" ", grp.Select(y => y.word))));
-            //label7.Text = string.Join(Environment.NewLine + "                    ", similarGames[3].Deck.ToString().Split().Select((word, index) => new { word, index }).GroupBy(y => y.index / 9).Select(grp => string.Join(" ", grp.Select(y => y.word))));
+            label5.Text = Game2.Deck.ToString();
+            label6.Text = Game3.Deck.ToString();
+            label7.Text = Game4.Deck.ToString();
 
         }
 
