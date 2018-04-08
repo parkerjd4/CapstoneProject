7846ac6226c6759d00e43080546287b6aa2c0269
Joseph Parker <parkerjd4@appstate.edu>
2018-04-08 14:31:51 -0400

    Changed the visuals of the add game menu.

--- GameLogger/GameLogger/Form1.cs
+++ GameLogger/GameLogger/Form1.cs
@@ -115,5 +115,10 @@ namespace GameLogger
         private void ComboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
